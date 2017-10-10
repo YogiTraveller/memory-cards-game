15a7ade66c8e138346b5e0464d4fc8f87c034f4c
Maciej Pienczewski <maciej.pienczewski@gmail.com>
2017-10-10 14:53:37 +0200

    Create cards varible & create match cards to these cards

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-var cards = ;
+
+// Create cards varible & create match cards to these cards
+var cards = ["bell", "camera", "car", "bolt", "heart", "star", "rocket", "usd"];
+var cardsWithMatch = [];
+
+for (i = 0; i < cards.length; i++) {
+  cardsWithMatch.push(cards[i]);
+  cardsWithMatch.push(cards[i] + "-match");
+};
+
+console.log(cards);
+console.log(cardsWithMatch);
 
 
 
